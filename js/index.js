84364195712e4bbd0189094cd65493fd1199fef1
mac <mac@esco-3.local>
2021-03-10 13:22:10 +0000

    init

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector(".menu")
+const sidebar = document.querySelector("#sidebar")
+const close = document.querySelector(".close")
+
+clear()
+menu.addEventListener("click", () => {
+
+        sidebar.classList.add("active")
+})
+
+
+close.addEventListener("click", function() {
+
+            clear()
+})
+
+
+function clear() {
+    sidebar.classList.remove("active")
+}
