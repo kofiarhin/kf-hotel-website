cd9d932120c30bddd7d6910fe0d8348809f41426
mac <mac@esco-3.local>
2021-03-15 14:28:09 +0000

    update: added animation to landing page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const menu = document.querySelector(".menu")
+function menuController() {
+
+    const menu = document.querySelector(".menu")
 const sidebar = document.querySelector("#sidebar")
 const close = document.querySelector(".close")
 
@@ -18,3 +20,28 @@ close.addEventListener("click", function() {
 function clear() {
     sidebar.classList.remove("active")
 }
+}
+
+
+function animationController() {
+
+        // check page
+        if(window.location.pathname === "/") {
+
+            setTimeout(() => {
+                
+                const title = document.querySelector("#landing #content h1")
+                const slug = document.querySelector(".slug")
+                const cta = document.querySelector("#content a")
+
+                
+            title.classList.add("active")
+            slug.classList.add("active")
+            cta.classList.add("active")
+
+            }, 3000)
+        }
+}
+
+menuController()
+animationController()
