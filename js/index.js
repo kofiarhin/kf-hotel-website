21547b33f5649d4543ed40a5f3958f86acf0f141
mac <mac@esco-3.local>
2021-03-15 15:15:32 +0000

    update

--- js/index.js
+++ js/index.js
@@ -25,23 +25,27 @@ function clear() {
 
 function animationController() {
 
+    console.log(window.location.pathname)
+
         // check page
-        if(window.location.pathname === "/") {
+        if(window.location.pathname === "/" || window.location.pathname === "/index.html") {
 
             setTimeout(() => {
                 
                 const title = document.querySelector("#landing #content h1")
                 const slug = document.querySelector(".slug")
                 const cta = document.querySelector("#content a")
+                const loader = document.querySelector(".loader")
 
-                
             title.classList.add("active")
             slug.classList.add("active")
             cta.classList.add("active")
+            loader.classList.add("active")
 
             }, 3000)
         }
 }
+
 
 menuController()
 animationController()
